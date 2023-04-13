f721649a36cd8afe5b7975f7be8e626bb57abe66
Kamilla <kamilla.kumysheva@mail.ru>
2023-04-13 13:52:28 +0300

    21

--- WpfApp4/Windows/WindowRegistration.xaml.cs
+++ WpfApp4/Windows/WindowRegistration.xaml.cs
@@ -22,6 +22,46 @@ namespace WpfApp4.Windows
         public WindowRegistration()
         {
             InitializeComponent();
+
+            CmbGender.ItemsSource = ClassHelper.EF.Context.Gender.ToList();
+            CmbGender.DisplayMemberPath = "GenderName";
+            CmbGender.SelectedIndex = 0;
+        }
+
+        private void BtnReg_Click(object sender, RoutedEventArgs e)
+        {
+            // валидация
+            if (string.IsNullOrWhiteSpace(TbLastName.Text))
+            {
+                MessageBox.Show("Поле Фамилия не заполнено");
+                return;
+            }
+
+            if (string.IsNullOrWhiteSpace(TbFirstName.Text))
+            {
+                MessageBox.Show("Поле Имя не заполнено");
+                return;
+            }
+            // добавление 
+            DB.Client addClient = new DB.Client();
+            addClient.Login = TbLogin.Text;
+            addClient.Password = PbPassword.Password;
+            addClient.Phone = TbPhone.Text;
+            addClient.FirstName = TbFirstName.Text;
+            addClient.LastName = TbLastName.Text;
+            if (TbMiddleName.Text != string.Empty)
+            {
+                addClient.Patronymic = TbMiddleName.Text;
+            }
+            addClient.GenderCode = (CmbGender.SelectedItem as DB.Gender).GenderCode;
+
+            ClassHelper.EF.Context.Client.Add(addClient);
+
+            // сохранение
+            ClassHelper.EF.Context.SaveChanges();
+
+            MessageBox.Show("Пользователь успешно добавлен");
+
         }
     }
 }
