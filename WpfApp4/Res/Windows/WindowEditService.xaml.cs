69c0464071409765743b8d3161f9c01786b8b4c4
Kamilla <kamilla.kumysheva@mail.ru>
2023-04-25 09:56:11 +0300

    1

---
+++ WpfApp4/Res/Windows/WindowEditService.xaml.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WpfApp4.Windows
+{
+    /// <summary>
+    /// Логика взаимодействия для WindowEditService.xaml
+    /// </summary>
+    public partial class WindowEditService : Window
+    {
+        DB.Service editService = null;
+
+        private bool isEdit = false;
+
+        public WindowEditService()
+        {
+            InitializeComponent();
+            isEdit = false;
+        }
+
+        public WindowEditService(DB.Service service)
+        {
+            InitializeComponent();
+
+            isEdit = true;
+
+            editService = service;
+
+            // Заполнение типа услуги
+
+            CmbTypeService.ItemsSource = ClassHelper.EF.Context.ServiceType.ToList();
+            CmbTypeService.DisplayMemberPath = "TypeName";
+
+            // выгрузка изображения 
+            ImgImageService.Source = new BitmapImage(new Uri(service.Image));
+
+            // заполнение полей
+            TbNameService.Text = service.ServiceName;
+            TbDiscService.Text = service.Description;
+            TbPriceService.Text = Convert.ToString(service.Price);
+
+            // заполнение типа услуги
+            CmbTypeService.SelectedItem = ClassHelper.EF.Context.ServiceType.Where(i => i.ID == service.ServiceTypeID).FirstOrDefault();
+
+        }
+
+        private void BtnEditService_Click(object sender, RoutedEventArgs e)
+        {
+
+
+            // валидация
+
+            editService.ServiceName = TbNameService.Text;
+            editService.Description = TbDiscService.Text;
+            editService.Price = Convert.ToDecimal(TbPriceService.Text);
+            editService.ServiceTypeID = (CmbTypeService.SelectedItem as DB.ServiceType).ID;
+
+            ClassHelper.EF.Context.SaveChanges();
+
+            MessageBox.Show("Данные успешно сохранены");
+
+            this.Close();
+        }
+
+        private void BtnEditService_Click_1(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
